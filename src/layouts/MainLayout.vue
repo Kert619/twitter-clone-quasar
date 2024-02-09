e9853abb61f9c7ac1ffaba22445971bd7e980dfc
Kert Tatoy <tatoy.kert.e@gmail.com>
2024-02-09 18:33:47 +0800

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,132 @@
+<template>
+  <q-layout view="lHr LpR fFf">
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          <span class="gt-xs">Twitter</span>
+          <q-icon
+            name="flight_takeoff"
+            size="sm"
+            color="primary"
+            class="lt-sm absolute-center"
+          />
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="283"
+    >
+      <q-icon name="flight_takeoff" size="lg" color="primary" class="q-pa-md" />
+
+      <q-list>
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple to="/about">
+          <q-item-section avatar>
+            <q-icon name="info" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold"
+            >About</q-item-section
+          >
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <q-input
+        v-model="searchText"
+        rounded
+        outlined
+        dense
+        label="Search"
+        class="q-ma-md"
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list padding separator>
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Technology</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Quasar is the best framework</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Technology</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Quasar is the best framework</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item class="q-pa-md">
+          <q-item-section>
+            <q-item-label overline class="text-grey">Technology</q-item-label>
+            <q-item-label class="text-weight-bold"
+              >Quasar is the best framework</q-item-label
+            >
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+const searchText = ref("");
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+</script>
